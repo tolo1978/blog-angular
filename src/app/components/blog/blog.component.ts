8b8e0f189eb1ed27f61f193484c4de0e6eaa7eab
Luis Llerena <luis.llerenaherraez@gmail.com>
2025-03-03 08:21:12 +0100

    Criterio 4 terminado

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { IPost } from '../../interfaces/ipost.interfaces';
 @Component({
   selector: 'app-blog',
   imports: [],
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-
+  
+  arrPosts: IPost[] = [
+    {
+      id: 1,
+      title: 'First Post',
+      url: 'https://example.com/first-post',
+      author: 'Author One',
+      date: '2023-01-01',
+      body: 'This is the body of the first post.'
+    },
+    {
+      id: 2,
+      title: 'Second Post',
+      url: 'https://example.com/second-post',
+      author: 'Author Two',
+      date: '2023-02-01',
+      body: 'This is the body of the second post.'
+    },
+    {
+      id: 3,
+      title: 'Third Post',
+      url: 'https://example.com/third-post',
+      author: 'Author Three',
+      date: '2023-03-01',
+      body: 'This is the body of the third post.'
+    }
+  ];
 }
---
+++ src/app/interfaces/ipost.interfaces.ts
@@ -0,0 +1,8 @@
+export interface IPost {
+    id?: number;
+    title: string;
+    url: string;
+    author: string;
+    date: string;
+    body: string;
+}
